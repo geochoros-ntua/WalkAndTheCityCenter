3cafc0c465dff5dee33436a4f027dc4eb900d346
pavlos tsagkis <p.tsagkis@gmail.com>
2021-03-29 23:05:16 +0300

    just a minor

--- src/backend/list_data.php
+++ src/backend/list_data.php
@@ -3,13 +3,12 @@
  * Returns the list of survey entries in excel format.
  */
 require 'connect.php';
-    
+
 $sql = "SELECT `id`,`firstname`,`lastname`,`country`,`organisation`,
 `city`,`age`,`email`,`Q1`,`Q2`,`Q3`,`Q4`,`Q5`,`Q6`,`Q7`,`Q8`,`Q9` FROM `survey`";  
 $setRec = mysqli_query($con, $sql);  
-$columnHeader = '';  
 $columnHeader = "Id" . "\t" . "First Name" . "\t" . "Last Name" . "\t". "Country" . "\t"
-. "City" . "\t". "Age" . "\t". "Email" . "\t". "Q1" . "\t". "Q2" . "\t". "Q3" . "\t". "Q4" . "\t"
+. "Organisation" . "\t". "City" . "\t". "Age" . "\t". "Email" . "\t". "Q1" . "\t". "Q2" . "\t". "Q3" . "\t". "Q4" . "\t"
 . "Q5" . "\t". "Q6" . "\t". "Q7" . "\t". "Q8" . "\t". "Q9" . "\t";  
 $setData = '';  
   while ($rec = mysqli_fetch_row($setRec)) {  
@@ -19,12 +18,12 @@ $setData = '';
         $rowData .= $value;  
     }  
     $setData .= trim($rowData) . "\n";  
-}  
-  
-header("Content-type: application/octet-stream");  
+} 
+$setData = $columnHeader . "\n" .  $setData;
+// such an ugly way to convert greek characters
+echo chr(255) . chr(254) . mb_convert_encoding($setData, 'UTF-16LE', 'UTF-8');
+header("Content-type: application/octet-stream; charset=utf-8");  
 header("Content-Disposition: attachment; filename=walk_survey.xls");  
 header("Pragma: no-cache");  
 header("Expires: 0");  
-
-  echo ucwords($columnHeader) . "\n" . $setData . "\n";  
- ?> 
+ ?>
